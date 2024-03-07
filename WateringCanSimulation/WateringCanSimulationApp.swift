6b5a58c70834055c3ddaede44aff6b5d5ae809b2
Dana Wakiji <dana@gromak.net>
2024-03-07 16:44:03 -0500

    Initial Commit

---
+++ WateringCanSimulation/WateringCanSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WateringCanSimulationApp.swift
+//  WateringCanSimulation
+//
+//  Created by Dana Wakiji on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WateringCanSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
